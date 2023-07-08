a1808db56a7014d84f3a966c221295064a3f6d8c
Jose Raphael Mercado <98337482+raphmercado@users.noreply.github.com>
2023-07-08 09:32:04 +0800

    Two changes
    Remove default value of input elem on click
    Return default value of input elem on focus out

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const inputNewTask = document.querySelector("#inputNewTask");
 const addNewTask = document.querySelector("#addNewTask");
 
 inputNewTask.addEventListener("click", removeDefaultValue);
+inputNewTask.addEventListener("focusout", returnDefaultValue);
 addNewTask.addEventListener("click", getNewTask);
 
 function removeDefaultValue(e) {
   console.log("Removing default value...");
   inputNewTask.setAttribute("value", "");
+}
+
+function returnDefaultValue(e) {
+  console.log("Returning default value...");
+  inputNewTask.setAttribute("value", "Add a new task");
+
 }
 
 function getNewTask(e) {
