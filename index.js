0626f4284d9a2fac2af52494524bdadbb4cee10e
Raph Mercado <98337482+raphmercado@users.noreply.github.com>
2023-08-21 02:48:22 +0800

    Add localStorage

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const newTaskInput = document.getElementById("newTaskInput");
 const addNewTaskBtn = document.getElementById("addNewTaskBtn");
 const taskCount = document.createElement("para");
-let taskCounter = 0;
 const taskList = [];
 
 function enableDisableButton() {
@@ -15,7 +14,6 @@ function enableDisableButton() {
 }
 
 function addNewTask() {
-  taskCounter++;
   console.log("Adding new task...");
   const task = document.createElement("li");
   task.textContent = newTaskInput.value;
@@ -23,8 +21,8 @@ function addNewTask() {
   taskList.push(newTaskInput.value);
   addNewTaskBtn.setAttribute("disabled", "");
   newTaskInput.value = "";
-  console.log(taskList);
-  console.log(taskCounter);
+  console.log(taskList.length);
+  localStorage.setItem("tasks", JSON.stringify(taskList));
   task.addEventListener("click", toggleChecked);
   main();
 }
@@ -33,22 +31,38 @@ function toggleChecked(e) {
   e.target.classList.toggle("checked");
 }
 
-function checkTaskCount() {
-  if (taskCounter === 0) {
+function displayTaskCount() {
+  if (taskList.length === 0) {
     taskCount.textContent = "You have no task."
-    taskListContainer.appendChild(taskCount);
-  } else if (taskCounter === 1) {
-    taskCount.textContent = `You have ${taskCounter} task.`;
-  } else if (taskCounter > 1) {
-    taskCount.textContent = `You have ${taskCounter} task/s.`;
+  } 
+  else if (taskList.length === 1) {
+    taskCount.textContent = `You have ${JSON.parse(localStorage.getItem("tasks")).length} task.`
+  } 
+  else if (taskList.length > 1) {
+    taskCount.textContent = `You have ${JSON.parse(localStorage.getItem("tasks")).length} tasks.`
   }
-  
-  }        
+}
+
+function displayTaskList() {
+  taskListContainer.appendChild(taskCount);
+  if (localStorage.length === 1) {
+    if (taskList.length === 0) {
+      for (let i = 0; i < JSON.parse(localStorage.getItem("tasks")).length; i++) {
+        taskList.push(JSON.parse(localStorage.getItem("tasks"))[i]);
+        const task = document.createElement("li");
+        task.textContent = JSON.parse(localStorage.getItem("tasks"))[i];
+        taskListContainer.appendChild(task);
+      }
+      console.log(taskList);
+    }
+  }
+}        
 
 function main() {
-  checkTaskCount();
+  displayTaskCount();
   newTaskInput.addEventListener("keyup", enableDisableButton);
   addNewTaskBtn.addEventListener("click", addNewTask);
 }
 
+window.onload = displayTaskList();
 main();
